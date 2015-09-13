634ea7ce50ca73519c50f894dbd53faadddbe723
Thiago Naves <naves.thiago@gmail.com>
2015-09-13 15:15:20 -0300

    Commited the right file this time xD

--- libraries/tests/meus/midi/main.cpp
+++ libraries/tests/meus/midi/main.cpp
@@ -1,73 +1,50 @@
 #include "mbed.h"
 #include "USBMIDI.h"
 
+AnalogIn pot(p20);
 USBMIDI midi;
-Serial pc(USBTX, USBRX);
 
-// MIDI IN
-void transmitMessage(MIDIMessage msg) {
- 	switch (msg.type()) {
-        case MIDIMessage::NoteOnType:
-        	wait(0.1);
-			midi.write(MIDIMessage::NoteOn(msg.key()));
-            break;
-        case MIDIMessage::NoteOffType:
-        	wait(0.1);
-			midi.write(MIDIMessage::NoteOff(msg.key()));
-            break;
-        case MIDIMessage::ProgramChangeType:
-        	wait(0.1);
-        	midi.write(MIDIMessage::ProgramChange(msg.program()));
-            break;
-        case MIDIMessage::SysExType:
-        	wait(0.1);
-        	unsigned char tmp[64];
-            for(int i=0;i<msg.length-1;i++) {
-	            tmp[i]=msg.data[i+1];
-            }
-            midi.write(MIDIMessage::SysEx(tmp,msg.length-1));
-            break;
-        default:
-            break;
-    }    
-}
+#define MEAN_COUNT 15
 
-int main(void)
+int samples[MEAN_COUNT];
+int count = 0;
+int oldest = 0;
+int last = -1;
+int main()
 {
-	wait(5);
-	// MIDI OUT
+//	wait(5);
+	while (1)
+	{
+		float v = pot.read();
+		int vi = v * 127;
+		int mean;
 
-	// set piano
-	midi.write(MIDIMessage::ProgramChange(1));
-	wait(0.1);
+		if (count < MEAN_COUNT)
+		{
+			samples[count] = vi;
+			count ++;
+		}
+		else
+		{
+			mean = 0;
+			for (int i=0; i<MEAN_COUNT; i++)
+				mean += samples[i];
 
-	// play A
-	midi.write(MIDIMessage::NoteOn(21));
-	wait(0.1);
-	midi.write(MIDIMessage::NoteOff(21));
-	wait(0.1);
+			mean /= MEAN_COUNT;
+			printf("-> %d\n", mean);
+			if (mean != last)
+			{
+				last = mean;
+				// Control, Value (0-127), channel
+				midi.write(MIDIMessage::ControlChange(1, mean, 0));
+			}
 
-	// GM reset
-	unsigned char gm_reset[]={0xF0,0x7E,0x7F,0x09,0x01,0xF7};
-	midi.write(MIDIMessage::SysEx(gm_reset,6));
-	wait(0.1);
+			samples[oldest] = vi;
+			oldest ++;
+			oldest = oldest % MEAN_COUNT;
 
-	// GM Master volume max
-	unsigned char gm_master_vol_max[]={0xF0,0x7F,0x7F,0x04,0x01,0x7F,0x7F,0xF7};
-	midi.write(MIDIMessage::SysEx(gm_master_vol_max,8));
-	wait(0.1);
-
-	// GS reset
-	unsigned char gs_reset[]={0xF0,0x41,0x10,0x42,0x12,0x40,0x00,0x7F,0x00,0x41,0xF7};
-	midi.write(MIDIMessage::SysEx(gs_reset,11));
-	wait(0.1);
-
-	// GS Master volume max
-	unsigned char gs_master_vol_max[]={0xF0,0x41,0x10,0x42,0x12,0x40,0x00,0x04,0x7F,0x3D,0xF7};
-	midi.write(MIDIMessage::SysEx(gs_master_vol_max,11));
-	wait(0.1);
-
-	midi.attach(transmitMessage);
-
-    while(1);
+			wait(0.005);
+		}
+	}
+	return 0;
 }
